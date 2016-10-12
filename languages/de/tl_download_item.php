131076bc9fb089725a6d4afc486cd3f64efcbf53
Thomas Kuhn <mail@th-kuhn.de>
2016-10-12 23:51:20 +0200

    Replaced html entities

--- languages/de/tl_download_item.php
+++ languages/de/tl_download_item.php
@@ -4,19 +4,19 @@ $GLOBALS['TL_LANG']['tl_download_item']['title_legend'] = 'Allgemein';
 $GLOBALS['TL_LANG']['tl_download_item']['protected_legend'] = 'Schutz';
 $GLOBALS['TL_LANG']['tl_download_item']['image_legend'] = 'Bilder';
 $GLOBALS['TL_LANG']['tl_download_item']['download_legend'] = 'Download';
-$GLOBALS['TL_LANG']['tl_download_item']['published_legend'] = 'Ver&ouml;ffentlichen';
+$GLOBALS['TL_LANG']['tl_download_item']['published_legend'] = 'Verööffentlichen';
 
-$GLOBALS['TL_LANG']['tl_download_item']['title'] = array('Bezeichnung', 'Geben Sie eine Bezeichnung f&uuml;r das Download-Element an.');
-$GLOBALS['TL_LANG']['tl_download_item']['type'] = array('Typ', 'W&auml;hlen Sie bitte den Typen aus.');
-$GLOBALS['TL_LANG']['tl_download_item']['teaser'] = array('Teaser', 'Geben Sie einen Teaser bzw. eine Kurzbeschreibung f&uuml;r das Download-Element ein.');
-$GLOBALS['TL_LANG']['tl_download_item']['text'] = array('Beschreibung', 'Geben Sie eine aussagekr&aumlftige Beschreibung f&uuml;r das Download-Element ein.');
-$GLOBALS['TL_LANG']['tl_download_item']['protected'] = array('Download-Element sch&uuml;tzen', 'M&ouml;chten Sie dieses Download-Element sch&uuml;tzen.');
-$GLOBALS['TL_LANG']['tl_download_item']['groups'] = array('Mitgliedergruppen', 'Bitte w&auml;hlen Sie die Mitgliedergruppen die Zugriff auf das Download-Element erhalten.');
-$GLOBALS['TL_LANG']['tl_download_item']['addImage'] = array('Bilder hinzuf&uuml;gen', 'M&ouml;chten Sie f&uuml;r das Download-Element ein Bild ausw&auml;hlen?');
-$GLOBALS['TL_LANG']['tl_download_item']['singleSRC'] = array('Vorschaubilder', 'W&auml;hlen Sie bitte Vorschaubilder f&uumlr; das Download-Element.');
-$GLOBALS['TL_LANG']['tl_download_item']['fileSRC'] = array('Dateien', 'W&auml;hlen Sie bitte die Dateien die heruntergeladen werden k&ouml;nnen.');
+$GLOBALS['TL_LANG']['tl_download_item']['title'] = array('Bezeichnung', 'Geben Sie eine Bezeichnung für das Download-Element an.');
+$GLOBALS['TL_LANG']['tl_download_item']['type'] = array('Typ', 'Wählen Sie bitte den Typen aus.');
+$GLOBALS['TL_LANG']['tl_download_item']['teaser'] = array('Teaser', 'Geben Sie einen Teaser bzw. eine Kurzbeschreibung für das Download-Element ein.');
+$GLOBALS['TL_LANG']['tl_download_item']['text'] = array('Beschreibung', 'Geben Sie eine aussagekr&aumlftige Beschreibung für das Download-Element ein.');
+$GLOBALS['TL_LANG']['tl_download_item']['protected'] = array('Download-Element schützen', 'Möchten Sie dieses Download-Element schützen.');
+$GLOBALS['TL_LANG']['tl_download_item']['groups'] = array('Mitgliedergruppen', 'Bitte wählen Sie die Mitgliedergruppen die Zugriff auf das Download-Element erhalten.');
+$GLOBALS['TL_LANG']['tl_download_item']['addImage'] = array('Bilder hinzufügen', 'Möchten Sie für das Download-Element ein Bild auswählen?');
+$GLOBALS['TL_LANG']['tl_download_item']['singleSRC'] = array('Vorschaubilder', 'Wählen Sie bitte Vorschaubilder f&uumlr; das Download-Element.');
+$GLOBALS['TL_LANG']['tl_download_item']['fileSRC'] = array('Dateien', 'Wählen Sie bitte die Dateien die heruntergeladen werden können.');
 
-$GLOBALS['TL_LANG']['tl_download_item']['published'] = array('Ver&ouml;ffentlichen', 'Soll das Download-Element ver&ouml;ffentlicht werden?');
+$GLOBALS['TL_LANG']['tl_download_item']['published'] = array('Veröffentlichen', 'Soll das Download-Element veröffentlicht werden?');
 $GLOBALS['TL_LANG']['tl_download_item']['start'] = array('Anzeigen ab', 'Das Download-Element erst ab diesem Tag auf der Webseite anzeigen.');
 $GLOBALS['TL_LANG']['tl_download_item']['stop'] = array('Anzeigen bis', 'Das Download-Element nur bis zu diesem Tag auf der Webseite anzeigen.');
 
