63cf2ba7bd10db208ea432b7e9c39d66560c5dcf
Thomas Kuhn <mail@th-kuhn.de>
2016-12-04 13:06:17 +0100

    Misc. stuff, removed unused stuff, added missing translations

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -5,9 +5,7 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['__selector__'][]      = 'download_r
 $GLOBALS['TL_DCA']['tl_module']['palettes']['download_archiv']     = '{title_legend},name,headline,type;{config_legend},download_archiv,jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['download_category']   = '{title_legend},name,headline,type;{template_legend:hide},category_template,customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['download_index']      = '{title_legend},name,headline,type;{config_legend},download_archiv,jumpTo;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
-$GLOBALS['TL_DCA']['tl_module']['palettes']['download_navigation'] = '{title_legend},name,headline,type;{nav_legend},download_levelOffset,download_showLevel,download_hardLimit;{list_legend},download_jumpTo,download_category_jumpTo;{reference_legend},download_reference;{template_legend:hide},customTpl,download_node_template;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
-
-$GLOBALS['TL_DCA']['tl_module']['subpalettes']['download_reference'] = 'download_referenceCategory';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['download_navigation'] = '{title_legend},name,headline,type;{nav_legend},download_levelOffset,download_showLevel,download_hardLimit;{jumpto_legend},download_jumpTo,download_category_jumpTo;{template_legend:hide},customTpl,download_node_template;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['download_jumpTo'] = array
 (
@@ -18,14 +16,14 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['download_jumpTo'] = array
 	'sql'                     => "int(10) NOT NULL default '0'"
 );
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['download_category_jumpTo'] = array
-(
-	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['download_jumpTo'],
-	'exclude'                 => true,
-	'inputType'               => 'pageTree',
-	'eval'                    => array('mandatory'=>true, 'fieldType'=>'radio', 'tl_class' => 'clr'),
-	'sql'                     => "int(10) NOT NULL default '0'"
-);
+//$GLOBALS['TL_DCA']['tl_module']['fields']['download_category_jumpTo'] = array
+//(
+//	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['download_category_jumpTo'],
+//	'exclude'                 => true,
+//	'inputType'               => 'pageTree',
+//	'eval'                    => array('mandatory'=>true, 'fieldType'=>'radio', 'tl_class' => 'clr'),
+//	'sql'                     => "int(10) NOT NULL default '0'"
+//);
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['download_levelOffset'] = array
 (
@@ -56,45 +54,6 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['download_hardLimit'] = array
 	'eval'                    => array('mandatory'=>false, 'isBoolean'=>true, 'tl_class'=>'clr'),
 	'sql'                     => "smallint(1) NOT NULL default '0'"
 );
-
-$GLOBALS['TL_DCA']['tl_module']['fields']['download_reference'] = array
-(
-	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['download_reference'],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'eval'                    => array('submitOnChange'=>true),
-	'sql'                     => "char(1) NOT NULL default ''"
-);
-
-$GLOBALS['TL_DCA']['tl_module']['fields']['download_referenceCategory'] = array
-(
-	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['download_referenceCategory'],
-	'exclude'                 => true,
-	'inputType'               => 'treePicker',
-	'eval'                    => array
-	(
-		'foreignTable'    => 'tl_download_structure',
-		'titleField'      => 'title',
-		'searchField'     => 'title',
-		'managerHref'     => 'do=theme&table=tl_module',
-		'fieldType'       => 'radio',
-		'multiple'        => false,
-		'pickerCallback'  => function($row) {
-			return $row['title'] . ' [' . $row['id'] . ']';
-		}
-	),
-	'sql'                     => "blob NULL"
-);
-
-//$GLOBALS['TL_DCA']['tl_module']['fields']['download_archiv'] = array
-//(
-//	'label'                   => &$GLOBALS['TL_LANG']['tl_download_archiv']['download_archiv'],
-//	'inputType'               => 'checkboxWizard',
-//	'exclude'                 => true,
-//	'foreignKey'              => 'tl_download_archiv.title',
-//	'eval'                    => array('mandatory'=>true, 'multiple'=>true),
-//	'sql'                     => "blob NOT NULL"
-//);
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['category_template'] = array
 (
